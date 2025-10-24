0ac308f04c72d0d0f65ef4b23aa618cba6673ca6
PoppyMan21 <samyar1724@gmail.com>
2025-10-24 16:53:58 +0330

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("لطفاً یک عدد وارد کنید: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            // شرط: هم کوچکتر از 10 باشد (num < 10)
+            //      و هم زوج باشد (num % 2 == 0)
+            if (num < 10 && num % 2 == 0)
+            {
+                Console.WriteLine("TRUE");
+            }
+        }
+    }
+}
